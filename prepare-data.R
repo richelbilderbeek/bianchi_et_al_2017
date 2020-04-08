f6963112d3fb3a917a65d382eacb0f44a3b4fda9
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-08 08:54:07 +0200

    Expose #1

--- prepare-data.R
+++ prepare-data.R
@@ -1,11 +1,11 @@
 # Converts the human proteome, as downloaded from UniProt,
 # into a more convenient data structure to work with: a named
-# list of strings. 
+# list of strings.
 
 # Also creates a few other files that are helpful for the analysis.
 
 library( seqinr )
-x <- read.fasta("proteome/UP000005640_9606.fasta.gz", 
+x <- read.fasta("proteome/UP000005640_9606.fasta.gz",
 	forceDNAtolower=FALSE, as.string=TRUE )
 x <- lapply( x, function(x) x[1] )
 names( x ) <- sapply( strsplit( names(x), "\\|" ), function(x) x[2] )
@@ -28,11 +28,11 @@ library( data.table )
 x <- read.table("tmh-predictions/trans-membrane-analysis-shortened.txt")
 x <- x[x$V2=="TMhelix",c(1,3,4)]
 
-x <- tapply( 1:nrow(x), list(x$V1), 
-	function(p) do.call( c, lapply( p, function(k) seq(x[k,2]-8,x[k,3])) )  ) 
+x <- tapply( 1:nrow(x), list(x$V1),
+	function(p) do.call( c, lapply( p, function(k) seq(x[k,2]-8,x[k,3])) )  )
 
 for( i in names(x) ){
-	x[[i]] <- intersect( x[[i]], 1:(nchar(proteome[[i]])-8) )
+	x[[i]] <- intersect( x[[i]], 1:(nchar(proteome[[i]])-8) ) # Bug in this line
 }
 
 for( i in names(proteome) ){
@@ -44,4 +44,4 @@ for( i in names(proteome) ){
 tmh.9mers <- x
 save( tmh.9mers, file="work/tmh.9mers.Rdata" )
 
-  
+
