59e07835bad6ef295e464573f4b38955cb098686
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-14 11:03:54 +0200

    Workaround for #6 for test run

--- correlate-to-hydrophobicity.R
+++ correlate-to-hydrophobicity.R
@@ -11,7 +11,7 @@ hyPref <- function( mhc="A02-01" ){
 	M <- 10^(-smmMatrix(mhc)$M)
 
 	M <- scale(M, center = FALSE, scale = colSums(M))
-  
+
 	colEntropies <- apply(M, 2, function(x) {
         	x <- x * log(x)
         	x[is.nan(x)] <- 0
@@ -30,19 +30,25 @@ par( bty="n", mar=c(4,4,.2,.2) )
 x <- 100* r[1,] / r[2,]
 y <- sapply( colnames(r), hyPref )+0.3963411
 
-plot( NA, 
+plot( NA,
 	xlab="Percentage of TMH epitopes",
 	ylab="Hydrophobic preference score",
 	xlim=c(2,15), ylim=c(-1.5,1.5) )
 
-abline( lm(y~x), col="gray")
+# In a test run, all x values are zeroes
+if (!all(x == 0.0) ) {
+  abline( lm(y~x), col="gray")
+}
 
 points( x, y, pch=19, cex=0.8 )
 
 text( x, y, substring( colnames(r), 1,3 ),
 	pos=c(1L, 2L, 4L, 4L, 2L, 4L, 4L, 4L, 1L, 4L, 4L, 2L, 4L), srt=0, cex=.8, offset=.3 )
 
-legend( "topleft", legend=bquote( r == .(signif(cor(x,y),2)) ), bty='n' )
+# In a test run, all x values are zeroes
+if (!all(x == 0.0) ) {
+  legend( "topleft", legend=bquote( r == .(signif(cor(x,y),2)) ), bty='n' )
+}
 
 dev.off()
 
