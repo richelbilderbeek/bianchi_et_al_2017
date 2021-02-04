d80feacb092c0765e8b453af7adc5539478de24e
richelbilderbeek <richel@richelbilderbeek.nl>
2021-02-04 16:05:38 +0100

    Fix test

--- tests/testthat/test-get_tmhmm_topology.R
+++ tests/testthat/test-get_tmhmm_topology.R
@@ -5,7 +5,7 @@ test_that("use", {
     tmhmm_filename = tmhmm_filename
   )
   expect_true(tibble::is_tibble(t))
-  expect_true(nrow(t) > 20000)
+  expect_equal(nrow(t), 2440)
   expect_equal(ncol(t), 2)
   expected_names <- c(
     "name",
@@ -23,7 +23,7 @@ test_that("proteome and topology has same names", {
   )
   expect_equal(nrow(proteome), nrow(topology))
   expect_true(tibble::is_tibble(t))
-  expect_true(nrow(t) > 20000)
+  expect_equal(nrow(t), 2440)
   expect_equal(ncol(t), 2)
   expected_names <- c(
     "name",
